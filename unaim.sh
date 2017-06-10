c7df1712ecd12b7eaa2eb67b7fc5acc24a348f5a
Brty.Liu <aim@liups.com>
2017-06-10 17:40:52 +0800

    modified:   unaim.sh

--- unaim.sh
+++ unaim.sh
@@ -5,7 +5,7 @@ sleep 20
 rm -rf ${DATADIR}
 rm -rf ${LOGDIR}
 rm -rf ${TMPDIR}
-rm -rf ${PRE_DATADIR}/my_${PORT}.cnf ${BASEDIR}/start_${PORT}.sh
+rm -rf ${PRE_DATADIR}/my_${PORT}.cnf ${BASEDIR}/start_${PORT}.sh ${BASEDIR}/stop_${PORT}.sh
 cp /etc/security/limits.conf.aimbk /etc/security/limits.conf
 cp /etc/profile.aimbk /etc/profile
 rm -rf /etc/security/limits.conf.aimbk /etc/profile.aimbk
